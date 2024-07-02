f2da26b4d1606bc179ebffb98ab7da784b4a1cb4
Rishav <rishavraj1707music@gmail.com>
2024-07-03 00:08:19 +0530

    livekit testing

---
+++ app/room/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const room = searchParams.get("roomId");
+  const name = searchParams.get("username");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await fetch(
+          `/api/get-participant-token?room=${room}&username=${name}`
+        );
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [name, room]);
+
+  if (token === "") {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <LiveKitRoom
+      video={true}
+      audio={true}
+      token={token}
+      connect={true}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      data-lk-theme="default"
+      style={{ height: "100dvh" }}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  );
+}
